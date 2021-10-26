48ea658695a5c5fae71a8baf3a6073836e69d3cc
recep3007 <90484341+recep3007@users.noreply.github.com>
2021-10-26 19:53:06 -0400

    Update hwrecep.java

--- src/Recep/hwrecep.java
+++ src/Recep/hwrecep.java
@@ -2,6 +2,7 @@ package Recep;
 
 public class hwrecep {
     public static void main(String[] args) {
-        System.out.println("hello git ");
-    }
+
+            System.out.println("hello git ");
+}
 }
